c93574570209b946bdfd79ae896040e1a29676d7
Artyom <vukolovartem228@gmail.com>
2023-03-24 22:51:52 +0100

    update

--- index.js
+++ index.js
@@ -1,110 +1,140 @@
 const TelegramApi = require('node-telegram-bot-api');
 const OpenAI = require('openai-api');
+const axios = require('axios');
+
 require('dotenv').config();
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const newsApiKey = process.env.NEWS_API_KEY;
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+let isCommand = false;
 const bot = new TelegramApi(token, { polling: true });
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const messageText = msg.text ? msg.text : '';
   
-    if (messageText.startsWith('/start')) {
-    //   bot.sendMessage(chatId, 'Ask me a question in any language you like, or use the /generate command');
-      bot.sendMessage(chatId, 'Ask me a question in any language you like');
+    // ***START***
+    if (messageText.startsWith('/start') || messageText.startsWith('/ыефке')) {
+        bot.sendMessage(chatId, 'Ask me a question in any language you like');
     } 
-    // else if (messageText.startsWith('/generate')) {
-    //   bot.sendMessage(chatId, 'Submit a photo to generate a similar one');
+    
+    // ***NEWS***
+    else if (messageText.startsWith('/news') || messageText.startsWith('/туцы') || messageText.startsWith('/nws')) {
+        isCommand = true;
 
-    //   const botInfo = await bot.getMe();
-    //   console.log(botInfo)
-    //     bot.once('photo', async (msg) => {
-    //     const processingMsg = await bot.sendMessage(chatId, 'Please wait, the photo request is being processed...');
-    //     try {
-    //         let photoUrl = '';
-    //         if (msg.photo && msg.photo.length > 0 && msg.photo[msg.photo.length - 1].file_id) {
-    //         const photo = await bot.getFile(msg.photo[msg.photo.length - 1].file_id);
-    //         photoUrl = photo.file_path ? `https://api.telegram.org/file/bot${token}/${photo.file_path}` : photo.file_url;
-    //         }
+        if (isCommand) {
+            bot.sendMessage(chatId, 'What are your interests?', {
+              reply_markup: {
+                inline_keyboard: [
+                    [
+                      { text: 'Tesla', callback_data: 'tesla' },
+                      { text: 'Sports', callback_data: 'sports' },
+                      { text: 'ChatGPT', callback_data: 'chatGPT' },
+                    ],
+                    [
+                      { text: 'Music', callback_data: 'music' },
+                      { text: 'Politics', callback_data: 'politics' },
+                      { text: 'Apple', callback_data: 'apple' },
+                    ],
+                    [
+                      { text: 'War', callback_data: 'war' },
+                      { text: 'Footbal', callback_data: 'football' },
+                      { text: 'Games', callback_data: 'games' },
+                    ],
+                ],
+                one_time_keyboard: true,
+              },
+            });
+        }
 
-    //         try {
-    //         const response = await openai.complete({
-    //             engine: 'davinci',
-    //             prompt: `Generate a photo similar to this: ${photoUrl}. Description: dog on the street`,
-    //             maxTokens: 1000,
-    //         });
-    //         if (response.data.outputs && response.data.outputs.length > 0) {
-    //             photoUrl = response.data.outputs[0].output_url;
-    //         }
-    //         bot.sendPhoto(chatId, photoUrl);
-    //         bot.editMessageText('Here is your generated photo:', { chat_id: chatId, message_id: processingMsg.message_id });
-    //         }catch(error){
-    //         console.error(error);
-    //         bot.editMessageText('Sorry, something went wrong while generating the photo. Please try again later. 0', { chat_id: chatId, message_id: processingMsg.message_id, text: '' });
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //         bot.editMessageText('Sorry, something went wrong while generating the photo. Please try again later.', { chat_id: chatId, message_id: processingMsg.message_id, text: '' });
-    //     }
-    //     });
-
-    // } 
-    else {
-      if (msg.photo && msg.photo.length > 0 && msg.photo[msg.photo.length - 1].file_id) { // проверка на наличие фото в сообщении
-        bot.sendMessage(chatId, 'To work with a photo, use the /generate command');
-      } else {
-        const processingMsg = await bot.sendMessage(chatId, '...');
-        // await analyzeDialog(0, 10, processingMsg, chatId, bot, openai);
-        const response = await openai.complete({
-            engine: 'text-davinci-003',
-            prompt: `${messageText}`,
-            maxTokens: 3000,
+        bot.once('callback_query', async (query) => {
+            const selectedInterest = query.data;
+            await bot.answerCallbackQuery(query.id);
+      
+            bot.sendMessage(chatId, 'In what language do you want to read the news?', {
+              reply_markup: {
+                inline_keyboard: [
+                  [
+                    { text: 'English', callback_data: 'en' },
+                    { text: 'Russian', callback_data: 'ru' },
+                    { text: 'Spanish', callback_data: 'es' },
+                  ],
+                //   [
+                //     { text: 'Chinese', callback_data: 'zh' },
+                //     { text: 'Kazakh', callback_data: 'kk' },
+                //     { text: 'Italian', callback_data: 'it' },
+                //   ],
+                //   [
+                //     { text: 'German', callback_data: 'de' },
+                //     { text: 'Japanese', callback_data: 'ja' },
+                //     { text: 'French', callback_data: 'fr' },
+                //   ],
+                ],
+                one_time_keyboard: true
+              },
+            });
+      
+            bot.once('callback_query', async (query) => {
+              const selectedLanguage = query.data;
+              await bot.answerCallbackQuery(query.id);
+      
+              getNews(chatId, selectedInterest, selectedLanguage);
+            });
         });
-        
-        const answer = response.data.choices[0].text.trim();
-        bot.editMessageText(`${answer}`, { chat_id: chatId, message_id: processingMsg.message_id });
-      }
-    }
-  });
+    } 
 
-bot.on('polling_error', async (error) => {
-  console.error(error);
+    // ***REGULAR DIALOGUE***
+    else {
+        if (msg.photo && msg.photo.length > 0 && msg.photo[msg.photo.length - 1].file_id) {
+            bot.sendMessage(chatId, 'To work with a photo, use the /generate command');
+        } 
+        else {
+            const processingMsg = await bot.sendMessage(chatId, '...');
+            const response = await openai.complete({
+                engine: 'text-davinci-003',
+                prompt: `${messageText}`,
+                maxTokens: 3000,
+            });
+            const answer = response.data.choices[0].text.trim();
+            bot.editMessageText(`${answer}`, { chat_id: chatId, message_id: processingMsg.message_id });
+        }
+
+    }
 });
 
-async function analyzeDialog(depth, maxDepth, processingMsg, chatId, bot, openai, dialog = '') {
+async function getNews(chatId, interests, language, pageSize = 10) {
     try {
-      const response = await openai.complete({
-        engine: 'text-davinci-003',
-        prompt: `${dialog}\n`,
-        maxTokens: 3000,
-      });
-      const nextBotMessage = response.data.choices[0].text.trim();
-  
-      await bot.editMessageText(`${nextBotMessage}`, { chat_id: chatId, message_id: processingMsg.message_id });
+        const response = await axios.get(`https://newsapi.org/v2/everything?language=${language}&q=${interests}&pageSize=${pageSize}&sortBy=publishedAt&apiKey=${newsApiKey}`);
+        const articles = response.data.articles;
 
-      if (depth >= maxDepth) {
-        return;
-      }
-
-      // Ждем ответа пользователя
-      const onMessage = async (msg) => {
-        if (msg.chat.id !== chatId) {
-          return;
+        if (articles.length === 0) {
+            bot.sendMessage(chatId, 'No articles found for your interests');
+            return;
         }
-        
-        const userMessage = msg.text.trim();
-        const newDialog = `${dialog}USER: ${userMessage}\n`;
-  
-        bot.off('message', onMessage);
-        await analyzeDialog(depth + 1, maxDepth, chatId, bot, openai, newDialog);
-      };
-  
-      bot.on('message', onMessage);
-    } 
-    catch (error) {
-      console.error(error);
-      await bot.sendMessage(chatId, 'Sorry, something went wrong while analyzing the dialog. Please try again later.');
+
+        for (let i = 0; i < articles.length; i++) {
+            const article = articles[i];
+            const message = `${article.title}\n\n${formatDate(article.publishedAt)}\n${article.url}\n\n`;
+            bot.sendMessage(chatId, message);
+        };
+
+    } catch (error) {
+        console.error(error);
+        bot.sendMessage(chatId, 'Sorry, something went wrong while fetching the news. Please try again later.');
     }
+}
+
+bot.on('polling_error', async (error) => {
+    console.error(error);
+});
+
+function formatDate(date) {
+    let init = date.substring(0, 10);
+    let year = init.substring(2, 4);
+    let month = init.substring(5, 7);
+    let day = init.substring(8, 10);
+    let res = `${day}.${month}.${year}`;
+    return res;
 }
